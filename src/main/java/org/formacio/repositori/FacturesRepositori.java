e5e7b59ca8d4001cb62c0a4ca13b3807c31e4183
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2020-06-01 15:03:52 +0200

    feat(FacturesRepositori): Añadir extension Repository+ Query totalClient

--- src/main/java/org/formacio/repositori/FacturesRepositori.java
+++ src/main/java/org/formacio/repositori/FacturesRepositori.java
@@ -1,8 +1,12 @@
 package org.formacio.repositori;
 
+import org.formacio.domain.Factura;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
 
-public interface FacturesRepositori  {
+public interface FacturesRepositori extends Repository<Factura, Long> {
 
+	@Query("select sum(linea.total) from Factura facturas join facturas.linies linea where facturas.client.nom = ?1")
 	public Number totalClient(String client);
 	
 }
