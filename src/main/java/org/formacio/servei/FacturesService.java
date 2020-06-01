1f60d034e4aa98215692b12913474dd05b061190
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2020-06-01 17:06:39 +0200

    feat(FacturesService): Añadir lógica afegirProducte + test OK

--- src/main/java/org/formacio/servei/FacturesService.java
+++ src/main/java/org/formacio/servei/FacturesService.java
@@ -1,12 +1,20 @@
 package org.formacio.servei;
 
+import java.util.Optional;
+
 import org.formacio.domain.Factura;
+import org.formacio.domain.LiniaFactura;
+import org.formacio.repositori.FacturesRepositori;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class FacturesService {
 
-	
+	@Autowired
+	private FacturesRepositori repositorioFacturas;
 	/*
 	 * Aquest metode ha de carregar la factura amb id idFactura i afegir una nova linia amb les dades
 	 * passades (producte i totalProducte)
@@ -16,7 +24,12 @@ public class FacturesService {
 	 * Per implementar aquest metode necessitareu una referencia (dependencia) a FacturesRepositori
 	 */
 	public Factura afegirProducte (long idFactura, String producte, int totalProducte) {
-		
-		return null;
+		Optional<Factura> factura = repositorioFacturas.findById(idFactura);
+		LiniaFactura nuevaLinea = new LiniaFactura();
+		nuevaLinea.setProducte(producte);
+		nuevaLinea.setTotal(totalProducte);
+		factura.get().getLinies().add(nuevaLinea);
+		repositorioFacturas.save(factura.get());
+		return factura.get();
 	}
 }
